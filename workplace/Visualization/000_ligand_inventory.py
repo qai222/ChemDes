57312d858026ec4edf415b4fa7df1fbad393627f
qai <qai1@fordham.edu>
2022-08-12 11:55:25 -0400

    place `smi2imagestr` in `lsal.utils`

--- workplace/Visualization/000_ligand_inventory.py
+++ workplace/Visualization/000_ligand_inventory.py
@@ -1,32 +1,16 @@
-import base64
 import os.path
-from io import BytesIO
 
 import dash_bootstrap_components as dbc
 import pandas as pd
 from dash import Dash
 from dash import html
-from rdkit.Chem import MolFromSmiles
-from rdkit.Chem.Draw import MolToImage
 
-
-def smi2imagestr(smi: str, to_html=True):
-    m = MolFromSmiles(smi)
-    img = MolToImage(m)
-    buffered = BytesIO()
-    img.save(buffered, format="JPEG")
-    encoded_image = base64.b64encode(buffered.getvalue())
-    src_str = 'data:image/png;base64,{}'.format(encoded_image.decode())
-    if to_html:
-        return (html.Img(src=src_str))
-    else:
-        return src_str
-
+from lsal.utils import smi2imagestr
 
 ligand_inventory_csv = "../Inventory/ligand_inventory.csv"
 df = pd.read_csv(ligand_inventory_csv)
 smiles = df.loc[:, "smiles"]
-df.loc[:, "Structure"] = [smi2imagestr(smi) for smi in smiles]
+df.loc[:, "Structure"] = [html.Img(src=smi2imagestr(smi)) for smi in smiles]
 
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
