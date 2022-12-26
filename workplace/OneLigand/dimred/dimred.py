850bba79d263d08b432356d9349a362cd7192244
qai <qai1@fordham.edu>
2022-12-26 05:41:31 -0500

    cleanup

--- workplace/OneLigand/dimred/dimred.py
+++ workplace/OneLigand/dimred/dimred.py
@@ -42,59 +42,53 @@ class DimRed(Worker):
 
     @log_time
     def load_df_dimred(self):
-        if file_exists(self.output):
-            dimred_df = pd.read_csv(self.output)
-            dimred_df['ligand_label'] = [li.label for li in self.ligands]
-            dimred_df = dimred_df.set_index('ligand_label')
-            dimred_df.to_csv(self.output)
-        else:
-            columns = []
-            values = []
-            for space in ['FEAT', 'CHEM']:
+        columns = []
+        values = []
+        for space in ['FEAT', 'CHEM']:
+            if space == 'FEAT':
+                tmp_dmat_npy = self.tmp_dmat_feat_npy
+            elif space == 'CHEM':
+                tmp_dmat_npy = self.tmp_dmat_chem_npy
+            else:
+                raise ValueError
+
+            if file_exists(tmp_dmat_npy):
+                logger.warning(f"found previously saved distance matrix: {tmp_dmat_npy}")
+                with open(tmp_dmat_npy, 'rb') as f:
+                    dmat = np.load(f)
+            else:
+                logger.info(f"cannot find previously saved distance matrix at {tmp_dmat_npy}")
                 if space == 'FEAT':
-                    tmp_dmat_npy = self.tmp_dmat_feat_npy
+                    _, feature_space = Molecule.l1_input(self.ligands, amounts=None)
+                    dmat = calculate_distance_matrix(feature_space, "manhattan")
                 elif space == 'CHEM':
-                    tmp_dmat_npy = self.tmp_dmat_chem_npy
+                    dmat = similarity_matrix([lig.smiles for lig in self.ligands], fp_type=self.fp_type)
                 else:
                     raise ValueError
+                with open(tmp_dmat_npy, 'wb') as f:
+                    np.save(f, dmat)
 
-                if file_exists(tmp_dmat_npy):
-                    logger.warning(f"found previously saved distance matrix: {tmp_dmat_npy}")
-                    with open(tmp_dmat_npy, 'rb') as f:
-                        dmat = np.load(f)
-                else:
-                    logger.info(f"cannot find previously saved distance matrix at {tmp_dmat_npy}")
-                    if space == 'FEAT':
-                        _, feature_space = Molecule.l1_input(self.ligands, amounts=None)
-                        dmat = calculate_distance_matrix(feature_space, "manhattan")
-                    elif space == 'CHEM':
-                        dmat = similarity_matrix([lig.smiles for lig in self.ligands], fp_type=self.fp_type)
-                    else:
-                        raise ValueError
-                    with open(tmp_dmat_npy, 'wb') as f:
-                        np.save(f, dmat)
+            assert dmat.shape == (len(self.ligands), len(self.ligands))
 
-                assert dmat.shape == (len(self.ligands), len(self.ligands))
-
-                values_space = []
-                for nnmd in self.dimred_params:
-                    nn, md = nnmd.split(";")
-                    nn = int(nn.replace("nn=", ""))
-                    md = float(md.replace("md=", ""))
-                    transformer = umap.UMAP(
-                        n_neighbors=nn, min_dist=md, metric="precomputed", random_state=SEED)
-                    data_2d = transformer.fit_transform(dmat)
-                    values_space.append(data_2d)
-                    columns += [f"DIMRED_{space}_{nnmd}_x", f"DIMRED_{space}_{nnmd}_y", ]
-                values_space = np.concatenate(values_space, axis=1)
-                assert values_space.shape == (len(self.ligands), 2 * len(self.dimred_params))
-                values.append(values_space)
-            values = np.concatenate(values, axis=1)
-            assert values.shape == (len(self.ligands), 2 * 2 * len(self.dimred_params))
-            dimred_df = pd.DataFrame(values, columns=columns)
-            dimred_df['ligand_label'] = [li.label for li in self.ligands]
-            dimred_df.set_index('ligand_label')
-            dimred_df.to_csv(self.output)
+            values_space = []
+            for nnmd in self.dimred_params:
+                nn, md = nnmd.split(";")
+                nn = int(nn.replace("nn=", ""))
+                md = float(md.replace("md=", ""))
+                transformer = umap.UMAP(
+                    n_neighbors=nn, min_dist=md, metric="precomputed", random_state=SEED)
+                data_2d = transformer.fit_transform(dmat)
+                values_space.append(data_2d)
+                columns += [f"DIMRED_{space}_{nnmd}_x", f"DIMRED_{space}_{nnmd}_y", ]
+            values_space = np.concatenate(values_space, axis=1)
+            assert values_space.shape == (len(self.ligands), 2 * len(self.dimred_params))
+            values.append(values_space)
+        values = np.concatenate(values, axis=1)
+        assert values.shape == (len(self.ligands), 2 * 2 * len(self.dimred_params))
+        dimred_df = pd.DataFrame(values, columns=columns)
+        dimred_df['ligand_label'] = [li.label for li in self.ligands]
+        dimred_df.set_index('ligand_label')
+        dimred_df.to_csv(self.output)
 
 
 if __name__ == '__main__':
@@ -105,7 +99,5 @@ if __name__ == '__main__':
         fp_type="ECFP4",
     )
     worker.run(
-        [
-            'load_df_dimred',
-        ]
+        ['load_df_dimred', ]
     )
