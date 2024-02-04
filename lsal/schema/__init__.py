eaddca59f6f245f25d6577f412e0eb7443afe0cd
qai <qai1@fordham.edu>
2024-02-03 23:23:03 -0500

    - add docs

--- lsal/schema/__init__.py
+++ lsal/schema/__init__.py
@@ -2,3 +2,7 @@ from .material import NanoCrystal, Molecule, featurize_molecules, load_molecules
 from .reaction import ReactionCondition, ReactantSolution, _EPS, L1XReactionCollection, L1XReaction, LXReaction, \
     assign_reaction_results
 from .workplace import Worker
+
+"""
+schema used for ligand exchange reactions
+"""
