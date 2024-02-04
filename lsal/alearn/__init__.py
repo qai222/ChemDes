73b4f6d99a68ddbd0a7a9795cad97fff9ff45600
qai <qai1@fordham.edu>
2024-02-03 23:00:49 -0500

    - add docs

--- lsal/alearn/__init__.py
+++ lsal/alearn/__init__.py
@@ -1,3 +1,7 @@
 from .base import TeachingRecord, QueryRecord
 from .one_ligand import SingleLigandLearner, SingleLigandPrediction, RankMethodDocs
 from .one_ligand_worker import OneLigandWorker
+
+"""
+base modules for ligand active learning
+"""
